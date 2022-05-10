7c65df53641261d41e0972bd80c7bb56ecb88c01
AMIT KUMAR BERA <84179065+itzwow@users.noreply.github.com>
2022-05-10 23:22:06 +0530

    day4

---
+++ Day4/217. Contains Duplicate.cpp
@@ -0,0 +1,53 @@
+/*PROBLEM STATEMENT:
+217. Contains Duplicate
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Brute force using sorting:
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1])return true;
+        }
+        return false;
+    }
+};
+
+// Optimal using hash map:
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       map<int, int> cntMap;
+        for(int i=0;i<nums.size();i++){
+            if(cntMap[nums[i]]){return true;}
+            cntMap[nums[i]]++;
+        }
+        return false;
+    }
+};
