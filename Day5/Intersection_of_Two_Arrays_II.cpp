a87a95541a4c435a618fda9dcde6835fc7678122
AMIT KUMAR BERA <84179065+itzwow@users.noreply.github.com>
2022-05-12 16:34:36 +0530

    day4

---
+++ Day5/Intersection_of_Two_Arrays_II.cpp
@@ -0,0 +1,51 @@
+/*PROBLEM STATEMENT:
+
+350. Intersection of Two Arrays II
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000         */
+
+#include<bits/stdc++.h>
+using  namespace std;
+
+// solution
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2= nums2.size();
+        
+        map <int, int> m;
+        vector<int>ans;
+        
+        for(int i=0;i<n1;i++){
+            m[nums1[i]]++;
+        }
+        
+        for(int i=0;i<n2;i++){
+            auto it = m.find(nums2[i]);
+            if(it != m.end() && it->second>0){
+                ans.push_back(nums2[i]);
+                it->second--;
+            }
+        }
+        return ans;
+        
+    }
+};
