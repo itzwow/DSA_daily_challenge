f321b24a88bc13cdc49a99f2c05693f3be928a62
AMIT KUMAR BERA <84179065+itzwow@users.noreply.github.com>
2022-05-31 00:40:26 +0530

    day16

--- Day16/Insert_An_Element_At_Its_Bottom_In_A_Given_Stack.cpp
+++ Day16/Insert_An_Element_At_Its_Bottom_In_A_Given_Stack.cpp
@@ -86,3 +86,25 @@ stack<int> pushAtBottom(stack<int>& myStack, int x)
     }
     return myStack;
 }
+
+// Recursive approach
+void solve(stack<int>& s, int x){
+       if(s.empty()){
+        s.push(x);
+        return ;
+    }
+    
+    int num = s.top();
+    s.pop();
+    
+//     recursive call
+    solve(s,x);
+    s.push(num);
+}
+
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    // Write your code here.
+    solve(myStack, x);
+     return myStack;
+}
